fe2c8f44afbfefaf743b2b174ed34db13aaeb919
wstcliyu <wstcliyu@gmail.com>
2018-05-20 15:37:50 +0800

    Add 476

--- NumberComplement_476.java
+++ NumberComplement_476.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class NumberComplement_476 {
+    public static int findComplement(int num) {
+        int first=0;
+        for (int i=31; i>=0; i--) {
+            if (((1<<i) & num) != 0) {
+                first = i;
+                break;
+            }
+        }
+        return (~num) & ((1<<first)-1);
+    }
+    public static void main (String args[]) {
+        int testNum = 1;
+        System.out.println(findComplement(testNum));
+    }
 }
