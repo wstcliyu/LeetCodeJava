7b95bd24b0a170e201b783e762153c222a3f975c
YuLi <wstcliyu@gmail.com>
2018-05-21 22:31:29 +0800

    regX of whitespace and dot

---
+++ Subdomain_811.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Subdomain_811 {
+    public static List<String> subdomainVisits(String[] cpdomains) {
+        // The regX of one or more space is \\s+
+        // The regX of . is \\.
+        Map<String, Integer> counts = new HashMap();
+        for (String domain: cpdomains) {
+            String[] cpinfo = domain.split("\\s+");
+            String[] frags = cpinfo[1].split("\\.");
+            int count = Integer.valueOf(cpinfo[0]);
+            String cur = "";
+            for (int i = frags.length - 1; i >= 0; --i) {
+                cur = frags[i] + (i < frags.length - 1 ? "." : "") + cur;
+                counts.put(cur, counts.getOrDefault(cur, 0) + count);
+            }
+        }
+
+        List<String> ans = new ArrayList();
+        for (String dom: counts.keySet())
+            ans.add(counts.get(dom) + " " + dom);
+        return ans;
+    }
+    public static void main (String args[]) {
+        String[] testCpdomains = {"900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"};
+        for (String s:subdomainVisits(testCpdomains))
+            System.out.println(s);
+    }
+}
