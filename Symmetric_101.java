fee43b9faa41b5e999c4b9db27d9d9b0142e1381
YuLi <wstcliyu@gmail.com>
2018-06-21 22:04:07 +0800

    101 Good practice for dfs and bfs

--- Symmetric_101.java
+++ Symmetric_101.java
@@ -21,4 +21,39 @@ public class Symmetric_101 {
             return false;
         return isMirrorPair(n1.left, n2.right) && isMirrorPair(n1.right, n2.left);
     }
+
+    // Standard solution #1 (Recursive), similar as mine but better writing
+    /*
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    public boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+        return (t1.val == t2.val)
+            && isMirror(t1.right, t2.left)
+            && isMirror(t1.left, t2.right);
+    }
+    */
+
+    /* Standard solution #2 (Iterative), with Queue to do bfs
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null) return false;
+            if (t1.val != t2.val) return false;
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
+    }
+    */
 }
