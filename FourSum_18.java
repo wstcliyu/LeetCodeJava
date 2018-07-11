1e15675cd70c0417b274468ff23414cd4e725740
YuLi <wstcliyu@gmail.com>
2018-07-11 22:36:05 +0800

    18 Using HashMap cannot make it faster

--- FourSum_18.java
+++ FourSum_18.java
@@ -4,16 +4,16 @@ import java.util.List;
 
 public class FourSum_18 {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
         List<List<Integer>> res = new LinkedList<>();
-        HashMap<Integer, ArrayList<Point>> map = new HashMap<>();
+        if (nums == null || nums.length < 4)
+            return res;
         int n = nums.length;
+        Arrays.sort(nums);
+        // My first solution using HashMap
+        /*
+        HashMap<Integer, ArrayList<Point>> map = new HashMap<>();
         for (int i=0; i<n-1; i++) {
-            if (i+1<n-1 && nums[i] == nums[i+1])
-                continue;
             for (int j=i+1; j<n; j++) {
-                if (j-1>=i+1 && nums[j] == nums[j-1])
-                    continue;
                 int sum = nums[i] + nums[j];
                 ArrayList<Point> tmp = new ArrayList<>();
                 if (map.containsKey(sum))
@@ -22,19 +22,59 @@ public class FourSum_18 {
                 map.put(sum, tmp);
             }
         }
-        for (int sum : map.keySet()) {
-            if (map.containsKey(target-sum)) {
-                for (Point p1 : map.get(sum)) {
-                    for (Point p2 : map.get(target-sum)) {
-                        if (p1.y < p2.x)
-                            res.add(Arrays.asList(nums[p1.x], nums[p1.y], nums[p2.x], nums[p2.y]));
+        for (int i=0; i<n-1; i++) {
+            if (i>0 && nums[i]==nums[i-1])
+                continue;
+            for (int j=i+1; j<n; j++) {
+                if (j>i+1 && nums[j]==nums[j-1])
+                    continue;
+                if (map.containsKey(target - nums[i] - nums[j])) {
+                    for (Point p : map.get(target - nums[i] - nums[j])) {
+                        if (p.x > j) {
+                            List<Integer> tmpList = res.peekLast();
+                            if (tmpList != null && nums[i] == tmpList.get(0) && nums[j] == tmpList.get(1) && nums[p.x] == tmpList.get(2) && nums[p.y] == tmpList.get(3))
+                                continue;
+                            res.add(Arrays.asList(nums[i], nums[j], nums[p.x], nums[p.y]));
+                        }
                     }
                 }
             }
         }
         return res;
+        */
+        for (int i=0; i<n-3; i++) {
+            if (i>0 && nums[i]==nums[i-1])
+                continue;
+            threeSumForFourSum(nums, target-nums[i], i+1, n-1, res, nums[i]);
+        }
+        return res;
+    }
+    private void threeSumForFourSum(int[] nums, int target, int low, int high, List<List<Integer>> ls, int first) {
+        for (int i=low; i<high-1; i++) {
+            if (i>low && nums[i]==nums[i-1])
+                continue;
+            twoSumForFourSum(nums, target-nums[i], i+1, high, ls, first, nums[i]);
+        }
+    }
+    private void twoSumForFourSum(int[] nums, int target, int low, int high, List<List<Integer>> ls, int first, int second) {
+        while (low < high) {
+            if (nums[low] + nums[high] < target)
+                low++;
+            else if (nums[low] + nums[high] > target)
+                high--;
+            else {
+                ls.add(Arrays.asList(first, second, nums[low], nums[high]));
+                do {
+                    low++;
+                } while(low < high && nums[low] == nums[low-1]);
+                do {
+                    high--;
+                } while(low < high && nums[high] == nums[high+1]);
+            }
+        }
     }
     public static void main (String args[]) {
+        // int[] testNums = {0,0,0,0};
         int[] testNums = {-3, -2, -1, 0, 0, 1, 2, 3};
         List<List<Integer>> testRes = new FourSum_18().fourSum(testNums, 0);
         for (List<Integer> i:testRes)
