4437eeab60da065dad9bdfa9e3970fd27c86d586
YuLi <wstcliyu@gmail.com>
2018-07-13 12:05:36 +0800

    147 Good submission, similar with mine, but better written and faster

--- Insertion_147.java
+++ Insertion_147.java
@@ -4,45 +4,6 @@ public class Insertion_147 {
         ListNode next;
         ListNode(int x) { val = x; }
     }
-    // Merge sort is faster than insertion sort
-    /*
-    public ListNode insertionSortList(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
-
-        ListNode slow = head;
-        ListNode slowPrev = null;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
-            slowPrev = slow;
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        slowPrev.next = null;
-
-        return merge(insertionSortList(head), insertionSortList(slow));
-    }
-
-    public ListNode merge(ListNode left, ListNode right) {
-        ListNode dummy = new ListNode(-1);
-        ListNode curr = dummy;
-        while (left != null && right != null) {
-            if (left.val < right.val) {
-                curr.next = left;
-                left = left.next;
-            }
-            else {
-                curr.next = right;
-                right = right.next;
-            }
-            curr = curr.next;
-        }
-        curr.next = left == null ? right : left;
-        return dummy.next;
-    }
-    */
 
     // Good submission, similar with mine, but better written and faster
     /*
