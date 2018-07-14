599c20bf8b7cfc49e1cecedbf677ff76b329269b
YuLi <wstcliyu@gmail.com>
2018-07-14 22:28:26 +0800

    416 Subset sum solved by DP

---
+++ Partition_416.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Partition_416 {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int n : nums)
+            sum += n;
+        if (sum % 2 != 0)
+            return false;
+        int s = sum/2;
+        int[] dp = new int[s + 1];
+        dp[0] = 1;
+        for (int n : nums) {
+            for (int i = s; i >= n; i--)
+                dp[i] |= dp[i - n];
+        }
+        return dp[s]==1;
+    }
+    public static void main (String args[]) {
+        int[] testNums = new int[100];
+        Arrays.fill(testNums, 100);
+        System.out.println(new Partition_416().canPartition(testNums));
+    }
+}
