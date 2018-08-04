86a8d0ef9b25433191790a85a01f80a017578700
YuLi <wstcliyu@gmail.com>
2018-08-04 15:06:56 +0800

    103 Read my annotation

---
+++ Binary_103.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Binary_103 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        LinkedList<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int depth = 0;
+        while (!q.isEmpty()) {
+            depth++;
+            // Cannot use List<Integer> level =
+            // Because there is no addFirst method for List
+            LinkedList<Integer> level = new LinkedList<>();
+            int len = q.size();
+            for (int i=0; i<len; i++) {
+                TreeNode tmp = q.poll();
+                if (depth % 2 == 1)
+                    level.add(tmp.val);
+                else
+                    level.addFirst(tmp.val);
+                if (tmp.left != null)
+                    q.add(tmp.left);
+                if (tmp.right != null)
+                    q.add(tmp.right);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
