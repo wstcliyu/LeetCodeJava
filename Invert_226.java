569be31a387aa9a6929067e9089046d42710f900
YuLi <wstcliyu@gmail.com>
2018-08-06 20:14:46 +0800

    226 Iteration(BFS), Recursion(DFS), Iteration(DFS)

--- Invert_226.java
+++ Invert_226.java
@@ -13,6 +13,7 @@ public class Invert_226 {
         // Similar to Binary_102
         // Time: O(n), Space: O(n)
         // n is the number of all the nodes
+        /*
         if (root == null)
             return root;
         LinkedList<TreeNode> q = new LinkedList<>();
@@ -32,8 +33,10 @@ public class Invert_226 {
             }
         }
         return root;
+        */
 
-        // Recursion
+
+        // Recursion (DFS)
         // Time: O(n), n is the number of all the nodes
         // Space: O(h). h is the height of the tree
         /*
@@ -44,5 +47,31 @@ public class Invert_226 {
         root.right = invertTree(tmp);
         return root;
         */
+
+
+        // Iteration (DFS)
+        // The above solution is correct, but it is also bound to the application stack,
+        // which means that it's no so much scalable -
+        // (you can find the problem size that will overflow the stack and crash your application),
+        // so more robust solution would be to use stack data structure.
+
+        if (root == null)
+            return root;
+
+        final LinkedList<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+
+        while(!stack.isEmpty()) {
+            final TreeNode node = stack.pop();
+            final TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+
+            if(node.left != null)
+                stack.push(node.left);
+            if(node.right != null)
+                stack.push(node.right);
+        }
+        return root;
     }
 }
