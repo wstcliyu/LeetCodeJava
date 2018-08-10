876444b8739547b4bbca8abe89537f2f20285b2c
wstcliyu <wstcliyu@gmail.com>
2018-08-10 18:37:22 +0800

    146 Needs improvement

--- LRU_146.java
+++ LRU_146.java
@@ -3,6 +3,82 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class LRU_146 {
+
+    public class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode pre;
+        DLinkedNode post;
+    }
+
+    private void removeNode(DLinkedNode node) {
+        DLinkedNode pre = node.pre;
+        DLinkedNode post = node.post;
+        pre.post = post;
+        post.pre = pre;
+    }
+
+    private DLinkedNode popTail() {
+        DLinkedNode res = tail.pre;
+        removeNode(res);
+        return res;
+    }
+
+    private void addNode(DLinkedNode node) {
+        node.pre = head;
+        node.post = head.post;
+
+        node.post.pre = node;
+        node.pre.post = node;
+    }
+
+    private int capacity;
+    private int count;
+    private DLinkedNode head;
+    private DLinkedNode tail;
+    private HashMap<Integer, DLinkedNode> cache;
+
+    public LRU_146(int capacity) {
+        this.capacity = capacity;
+        count = 0;
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head.post = tail;
+        tail.pre = head;
+        cache = new HashMap<>();
+    }
+
+    public int get(int key) {
+        if (!cache.containsKey(key))
+            return -1;
+        DLinkedNode node = cache.get(key);
+        removeNode(node);
+        addNode(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (!cache.containsKey(key)) {
+            DLinkedNode newNode = new DLinkedNode();
+            newNode.key = key;
+            newNode.value = value;
+            cache.put(key, newNode);
+            addNode(newNode);
+            if (++count > capacity) {
+                cache.remove(popTail().key);
+                count--;
+            }
+        }
+        else {
+            DLinkedNode node = cache.get(key);
+            node.value = value;
+            removeNode(node);
+            addNode(node);
+        }
+    }
+
+    // My first solution
+    /*
     HashMap<Integer, Integer> map;
     Queue<Integer> q;
     int empty;
@@ -35,4 +111,5 @@ public class LRU_146 {
             }
         }
     }
+    */
 }
