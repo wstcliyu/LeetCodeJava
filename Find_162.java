177064535314c8b8401bb146ee98d0a657b44520
YuLi <wstcliyu@gmail.com>
2018-08-17 16:28:23 +0800

    162 Read my code

---
+++ Find_162.java
@@ -0,0 +1,32 @@
+public class Find_162 {
+    // Standard Solution Approach #2 Recursive
+    public int findPeakElement(int[] nums) {
+        return search(nums, 0, nums.length - 1);
+    }
+
+    private int search(int[] nums, int l, int r) {
+        if (l == r)
+            return l;
+        int mid = (l + r) / 2;
+        if (nums[mid] > nums[mid + 1])
+            return search(nums, l, mid);
+        else
+            return search(nums, mid + 1, r);
+    }
+
+
+    // Standard Solution Approach #3 Iterative
+    /*
+    public int findPeakElement(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+    */
+}
