120c2635cee6a5d75bffd238d794c94fa9a62e90
YuLi <wstcliyu@gmail.com>
2018-08-19 00:12:22 +0800

    154 Refer to 153, 33, 81

---
+++ Find_154.java
@@ -0,0 +1,19 @@
+public class Find_154 {
+
+    // Refer to 153, 33, 81
+    // Most voted solution
+
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[r])
+                l = mid + 1;
+            else if (nums[mid] < nums[r])
+                r = mid;
+            else
+                r--;
+        }
+        return nums[l];
+    }
+}
