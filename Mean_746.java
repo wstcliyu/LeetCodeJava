43ecabe0d36592da968b4c7ebafb0f853a0e4978
YuLi <wstcliyu@gmail.com>
2018-09-01 07:39:20 -0400

    746 Simple DP

---
+++ Mean_746.java
@@ -0,0 +1,9 @@
+public class Mean_746 {
+    public int minCostClimbingStairs(int[] cost) {
+        int len = cost.length;
+        int[] dp = new int[len + 1];
+        for (int i=2; i<=len; i++)
+            dp[i] = Math.min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
+        return dp[len];
+    }
+}
