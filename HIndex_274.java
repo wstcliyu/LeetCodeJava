6713064044500d2353483d506365e2f66d10c57a
wstcliyu <wstcliyu@gmail.com>
2018-09-28 23:37:55 -0400

    274 Second voted solution

---
+++ HIndex_274.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class HIndex_274 {
+    public static int hIndex(int[] citations) {
+        // Second voted solution
+        int n = citations.length;
+        int[] buckets = new int[n + 1];
+        for (int c : citations) {
+            if (c >= n)
+                buckets[n]++;
+            else
+                buckets[c]++;
+        }
+        int count = 0;
+        for (int i = n; i >= 0; i--) {
+            count += buckets[i];
+            if (count >= i)
+                return i;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] testCitations = {1};
+        System.out.println(hIndex(testCitations));
+    }
+}
