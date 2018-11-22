b4dfa84ed9b98b83068d9caf0ad7b073ef5ca55c
wstcliyu <wstcliyu@gmail.com>
2018-11-21 21:59:17 -0500

    938 dfs recursion iteration

---
+++ Range_938.java
@@ -0,0 +1,60 @@
+import java.util.Stack;
+
+public class Range_938 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    // Recursion
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if (root == null) return 0; // base case.
+        if (root.val < L) return rangeSumBST(root.right, L, R); // left branch excluded.
+        if (root.val > R) return rangeSumBST(root.left, L, R); // right branch excluded.
+        return root.val + rangeSumBST(root.right, L, R) + rangeSumBST(root.left, L, R); // count in both children.
+    }
+
+    // Iteration
+    /*
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        int ans = 0;
+        Stack<TreeNode> stack = new Stack();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node != null) {
+                if (L <= node.val && node.val <= R)
+                    ans += node.val;
+                if (L < node.val)
+                    stack.push(node.left);
+                if (node.val < R)
+                    stack.push(node.right);
+            }
+        }
+        return ans;
+    }
+    */
+
+    // Another version of recursion
+    /*
+    int ans;
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        ans = 0;
+        dfs(root, L, R);
+        return ans;
+    }
+
+    public void dfs(TreeNode node, int L, int R) {
+        if (node != null) {
+            if (L <= node.val && node.val <= R)
+                ans += node.val;
+            if (L < node.val)
+                dfs(node.left, L, R);
+            if (node.val < R)
+                dfs(node.right, L, R);
+        }
+    }
+    */
+}
