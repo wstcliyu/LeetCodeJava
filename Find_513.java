9d10eb69ab3bc372fda773a102928660cca8db5a
wstcliyu <wstcliyu@gmail.com>
2018-11-23 00:02:18 -0500

    513 BFS, right to left

--- Find_513.java
+++ Find_513.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.LinkedList;
 
 public class Find_513 {
+    // BFS, right to left
+    public int findBottomLeftValue(TreeNode root) {
+        LinkedList<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            root = q.remove();
+            if (root.right != null) q.add(root.right);
+            if (root.left != null) q.add(root.left);
+        }
+        return root.val;
+    }
 }
