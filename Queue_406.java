b5ab09f4460c9fcb0c5de32d0de56c01aee7bba6
wstcliyu <wstcliyu@gmail.com>
2018-11-22 19:05:19 -0500

    406 Refer to the link in my annotation

--- Queue_406.java
+++ Queue_406.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Queue_406 {
+    // https://leetcode.com/problems/queue-reconstruction-by-height/discuss/89359/Explanation-of-the-neat-Sort%2BInsert-solution
+
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b) -> (a[0] != b[0] ? b[0] - a[0] : a[1] - b[1]));
+        ArrayList<int[]> res = new ArrayList<>();
+        for (int[] p : people)
+            res.add(p[1], p);
+        return res.toArray(new int[0][0]);
+    }
 }
