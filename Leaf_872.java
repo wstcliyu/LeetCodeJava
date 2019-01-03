5fa2cd18cb2f5492008cdb340962e6b70d585c36
wstcliyu <wstcliyu@gmail.com>
2019-01-03 17:58:31 -0500

    872 Add most voted solution

--- Leaf_872.java
+++ Leaf_872.java
@@ -30,4 +30,25 @@ public class Leaf_872 {
         leafValueSequence(root.left, s);
         leafValueSequence(root.right, s);
     }
+
+    // Most voted solution
+    // https://leetcode.com/problems/leaf-similar-trees/discuss/152329/C%2B%2BJavaPython-O(logN)-Space
+    /*
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        Stack<TreeNode> s1 = new Stack<>(), s2 = new Stack<>();
+        s1.push(root1); s2.push(root2);
+        while (!s1.empty() && !s2.empty())
+            if (dfs(s1) != dfs(s2)) return false;
+        return s1.empty() && s2.empty();
+    }
+
+    public int dfs(Stack<TreeNode> s) {
+        while (true) {
+            TreeNode node = s.pop();
+            if (node.right != null) s.push(node.right);
+            if (node.left != null) s.push(node.left);
+            if (node.left == null && node.right == null) return node.val;
+        }
+    }
+    */
 }
