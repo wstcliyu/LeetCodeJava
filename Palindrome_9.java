6a5f1830ef1cbafeb17d0bd689d09ab6e0914ac4
wstcliyu <wstcliyu@gmail.com>
2019-01-08 13:02:25 -0500

    9 Add comment

--- Palindrome_9.java
+++ Palindrome_9.java
@@ -1,14 +1,13 @@
 public class Palindrome_9 {
     public boolean isPalindrome(int x) {
-        if (x<0 || (x%10==0 && x!=0))
+        // compare half of the digits in x, so don't need to deal with overflow
+        if (x < 0 || (x % 10 == 0 && x != 0))
             return false;
         int revert = 0;
-        while (x>revert) {
+        while (x > revert) {
             revert = revert * 10 + x % 10;
             x = x/10;
         }
-        if (x == revert || x == revert/10)
-            return true;
-        return false;
+        reurn x == revert || x == revert/10;
     }
 }
