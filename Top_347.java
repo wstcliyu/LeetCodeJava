9994787281e41472772ffd9b89fe39d205c283f0
wstcliyu <wstcliyu@gmail.com>
2019-01-10 10:37:32 -0500

    347 Review priority queue

--- Top_347.java
+++ Top_347.java
@@ -4,8 +4,59 @@ import java.util.HashSet;
 import java.util.List;
 
 public class Top_347 {
+
+    // Standard solution using Heap
+    // Time: O(Nlog(k)). The complexity of Counter method is O(N). To build a heap and output list takes O(Nlog(k)). 
+    // Hence the overall complexity of the algorithm is O(N+Nlog(k))=O(Nlog(k))
+    // Space : O(N) to store the hash map
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        // build hash map : character and how often it appears
+        HashMap<Integer, Integer> count = new HashMap();
+        for (int n: nums) 
+            count.put(n, count.getOrDefault(n, 0) + 1);
+
+        // init heap 'the less frequent element first'
+        PriorityQueue<Integer> heap =
+                new PriorityQueue<Integer>((n1, n2) -> count.get(n1) - count.get(n2));
+
+        // keep k top frequent elements in the heap
+        for (int n: count.keySet()) {
+            heap.add(n);
+            if (heap.size() > k)
+                heap.poll();
+        }
+
+        // build output list
+        List<Integer> top_k = new LinkedList();
+        while (!heap.isEmpty())
+            top_k.add(heap.poll());
+
+        Collections.reverse(top_k);
+        return top_k;
+    }
+
+    // My solution using priority queue
+    /*
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> count = new HashMap();
+        for (int n: nums) 
+            count.put(n, count.getOrDefault(n, 0) + 1);
+
+        PriorityQueue<Integer> heap =
+                new PriorityQueue<Integer>((n1, n2) -> count.get(n2) - count.get(n1));
+        heap.addAll(count.keySet());
+
+        List<Integer> top_k = new LinkedList();
+        for (int i = 0; i < k; i++)
+            top_k.add(heap.poll());
+        return top_k;
+    }
+    */
+
+
     // Most voted solution
     // Time: O(n)
+    /*
     public List<Integer> topKFrequent(int[] nums, int k) {
         HashSet<Integer>[] bucket = new HashSet[nums.length + 1];
         HashMap<Integer, Integer> freq = new HashMap<>();
@@ -24,4 +75,5 @@ public class Top_347 {
         }
         return res;
     }
+    */
 }
