e988a1474af18bb8b2b19195f7a6e7f0addfe076
wstcliyu <wstcliyu@gmail.com>
2019-01-10 13:08:48 -0500

    187 Minor change “”

--- Repeated_187.java
+++ Repeated_187.java
@@ -16,8 +16,7 @@ public class Repeated_187 {
 
         for(int i = 0; i < s.length() - 9; i++) {
             int v = 0;
-            for(int j = i; j < i + 10; j++) {
-                v <<= 2;
+            for(int j = i; j < i + 10; j++, v <<= 2) {
                 v |= map[s.charAt(j) - 'A'];
             }
             if(!words.add(v) && doubleWords.add(v))
