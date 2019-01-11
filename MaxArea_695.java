df6089f21a2e94c1caac68e17592f77d7f696f6d
wstcliyu <wstcliyu@gmail.com>
2019-01-11 11:42:11 -0500

    695 Add standard solution

--- MaxArea_695.java
+++ MaxArea_695.java
@@ -1,4 +1,30 @@
 public class MaxArea_695 {
+    // Standard Solution
+    int[][] grid;
+    boolean[][] seen;
+
+    public int area(int r, int c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length ||
+                seen[r][c] || grid[r][c] == 0)
+            return 0;
+        seen[r][c] = true;
+        return (1 + area(r+1, c) + area(r-1, c)
+                  + area(r, c-1) + area(r, c+1));
+    }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        this.grid = grid;
+        seen = new boolean[grid.length][grid[0].length];
+        int ans = 0;
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                ans = Math.max(ans, area(r, c));
+            }
+        }
+        return ans;
+    }
+    // My first solution
+    /*
     public static int maxAreaOfIsland(int[][] grid) {
         int res = 0;
         for (int i=0; i<grid.length; i++) {
@@ -15,6 +41,8 @@ public class MaxArea_695 {
         grid[x][y] = 0;
         return 1 + helper(grid, x+1, y) + helper(grid, x-1, y) + helper(grid, x, y+1) + helper(grid, x, y-1);
     }
+    */
+
     public static void main (String[] args) {
         int[][] testGrid = {{0,0,1,0,0,0,0,1,0,0,0,0,0},
                             {0,0,0,0,0,0,0,1,1,1,0,0,0},
