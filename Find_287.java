3790003a9fe978b8936661e2cb920d1d23482188
wstcliyu <wstcliyu@gmail.com>
2019-01-16 21:07:41 -0500

    287 Add solution using binary search

--- Find_287.java
+++ Find_287.java
@@ -14,4 +14,24 @@ public class Find_287 {
         }
         return slow;
     }
+
+
+    // Binary Search
+    // https://leetcode.com/problems/find-the-duplicate-number/discuss/72844/Two-Solutions-(with-explanation)%3A-O(nlog(n))-and-O(n)-time-O(1)-space-without-changing-the-input-array
+    /*
+    public int findDuplicate(int[] nums) {
+        int left = 1, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            int count = 0;
+            for (int num : nums) 
+                if (num <= mid) count++;
+            if (count <= mid)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+    */
 }
