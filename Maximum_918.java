0bc3322878031e74fca472e47ff3a5576f3b3296
wstcliyu <wstcliyu@gmail.com>
2019-01-16 21:07:15 -0500

    918 Multiple solutions including monoqueue

---
+++ Maximum_918.java
@@ -0,0 +1,137 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Maximum_918 {
+	// Most voted solution (similar with mine)
+	// https://leetcode.com/problems/maximum-sum-circular-subarray/discuss/178422/C%2B%2BJavaPython-One-Pass
+	public int maxSubarraySumCircular(int[] A) {
+        int total = 0, curMax = 0, curMin = 0;
+        int maxSum = Integer.MIN_VALUE, minSum = Integer.MAX_VALUE;
+        for (int a : A) {
+            curMax = a + Math.max(curMax, 0);
+            curMin = a + Math.min(curMin, 0);
+            maxSum = Math.max(maxSum, curMax);
+            minSum = Math.min(minSum, curMin);
+            total += a;
+        }
+        // One corner case to pay attention: If all number are non-positive,
+		// return the maximum one, (which equals to the max subarray sum).
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+    }
+
+
+    // Standard solution approach #2: Prefix Sums + Monoqueue
+    /*
+    public int maxSubarraySumCircular(int[] A) {
+        int N = A.length;
+
+        // Compute P[j] = B[0] + B[1] + ... + B[j-1]
+        // for fixed array B = A+A
+        int[] P = new int[2*N+1];
+        for (int i = 0; i < 2*N; ++i)
+            P[i+1] = P[i] + A[i % N];
+
+        // Want largest P[j] - P[i] with 1 <= j-i <= N
+        // For each j, want smallest P[i] with i >= j-N
+        int ans = A[0];
+        // deque: i's, increasing by P[i]
+        Deque<Integer> deque = new ArrayDeque();
+        deque.offer(0);
+
+        for (int j = 1; j <= 2*N; ++j) {
+            // If the smallest i is too small, remove it.
+            if (deque.peekFirst() < j-N)
+                deque.pollFirst();
+
+            // The optimal i is deque[0], for cand. answer P[j] - P[i].
+            ans = Math.max(ans, P[j] - P[deque.peekFirst()]);
+
+            // Remove any i1's with P[i2] <= P[i1].
+            while (!deque.isEmpty() && P[j] <= P[deque.peekLast()])
+                deque.pollLast();
+
+            deque.offerLast(j);
+        }
+
+        return ans;
+    }
+    */
+
+    // Standard solution approach #4
+    /*
+    public int maxSubarraySumCircular(int[] A) {
+        // S: sum of A
+        int S = 0;
+        for (int x: A)
+            S += x;
+
+        // ans1: answer for one-interval subarray
+        int ans1 = Integer.MIN_VALUE;
+        int cur = Integer.MIN_VALUE;
+        for (int x: A) {
+            cur = x + Math.max(cur, 0);
+            ans1 = Math.max(ans1, cur);
+        }
+
+        // ans2: answer for two-interval subarray, interior in A[1:]
+        int ans2 = Integer.MAX_VALUE;
+        cur = Integer.MAX_VALUE;
+        for (int i = 1; i < A.length; ++i) {
+            cur = A[i] + Math.min(cur, 0);
+            ans2 = Math.min(ans2, cur);
+        }
+        ans2 = S - ans2;
+
+        // ans3: answer for two-interval subarray, interior in A[:-1]
+        int ans3 = Integer.MAX_VALUE;
+        cur = Integer.MAX_VALUE;
+        for (int i = 0; i < A.length - 1; ++i) {
+            cur = A[i] + Math.min(cur, 0);
+            ans3 = Math.min(ans3, cur);
+        }
+
+        return Math.max(ans1, Math.max(ans2, ans3));
+    }
+    */
+
+
+    // Standard solution approach #1
+    /*
+    public int maxSubarraySumCircular(int[] A) {
+        int N = A.length;
+
+        int ans = A[0], cur = A[0];
+        for (int i = 1; i < N; ++i) {
+            cur = A[i] + Math.max(cur, 0);
+            ans = Math.max(ans, cur);
+        }
+
+        // ans is the answer for 1-interval subarrays.
+        // Now, let's consider all 2-interval subarrays.
+        // For each i, we want to know
+        // the maximum of sum(A[j:]) with j >= i+2
+
+        // rightsums[i] = A[i] + A[i+1] + ... + A[N-1]
+        int[] rightsums = new int[N];
+        rightsums[N-1] = A[N-1];
+        for (int i = N-2; i >= 0; --i)
+            rightsums[i] = rightsums[i+1] + A[i];
+
+        // maxright[i] = max_{j >= i} rightsums[j]
+        int[] maxright = new int[N];
+        maxright[N-1] = A[N-1];
+        for (int i = N-2; i >= 0; --i)
+            maxright[i] = Math.max(maxright[i+1], rightsums[i]);
+
+        int leftsum = 0;
+        for (int i = 0; i < N-2; ++i) {
+            leftsum += A[i];
+            ans = Math.max(ans, leftsum + maxright[i+2]);
+        }
+
+        return ans;
+    }
+    */
+
+
+}
