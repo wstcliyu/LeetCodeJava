6c7bf7aa5fda84deaf96916f6f9c7deff0865130
wstcliyu <wstcliyu@gmail.com>
2019-01-23 00:01:13 -0500

    445 Use stack to reverse linkedlist

---
+++ Add_445.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class Add_445 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    // Use stack to reverse linkedlist
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+        while (l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+        int sum = 0;
+        ListNode head = new ListNode(0);
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) sum += s1.pop();
+            if (!s2.empty()) sum += s2.pop();
+            head.val = sum % 10;
+            sum /= 10;
+            ListNode newHead = new ListNode(sum);
+            newHead.next = head;
+            head = newHead;
+        }
+        return head.val == 0 ? head.next : head;
+    }
+}
