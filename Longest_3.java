7de67635388a55c1962b2ef8a258d08eedaaffef
wstcliyu <wstcliyu@gmail.com>
2019-01-29 17:26:59 -0500

    3 sliding window or DP

--- Longest_3.java
+++ Longest_3.java
@@ -1,8 +1,27 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class Longest_3 {
+    // DP with a map storing the index of last appearance of the current character
     public int lengthOfLongestSubstring(String s) {
-        /* Two Pointers, Sliding Window
+        if (s == null || s.length() == 0) return 0;
+        int res = 0, start = -1;
+        // int[] index = new int[128]; // int[128(256)] for (Extended) ASCII
+        // Arrays.fill(index, -1);
+        HashMap<Character, Integer> index = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            // start = Math.max(start, index[s.charAt(i)]);
+            start = Math.max(start, index.getOrDefault(s.charAt(i), -1));
+            res = Math.max(res, i - start);
+            // index[s.charAt(i)] = i;
+            index.put(s.charAt(i), i);
+        }
+        return res;
+    }
+
+
+    // Sliding window using set
+    /*
+    public int lengthOfLongestSubstring(String s) {
         int n = s.length();
         Set<Character> set = new HashSet<>();
         int ans = 0, i = 0, j = 0;
@@ -15,20 +34,32 @@ public class Longest_3 {
                 set.remove(s.charAt(i++));
         }
         return ans;
-        */
-        if (s == null || s.length() == 0)
-            return 0;
-        int res = 0, start = -1;
-        // int[] index = new int[128]; // int[128(256)] for (Extended) ASCII
-        // Arrays.fill(index, -1);
-        HashMap<Character,Integer> index = new HashMap<>();
-        for (int i=0; i<s.length(); i++) {
-            // start = Math.max(start, index[s.charAt(i)]);
-            start = Math.max(start, index.getOrDefault(s.charAt(i), -1));
-            res = Math.max(res, i-start);
-            // index[s.charAt(i)] = i;
-            index.put(s.charAt(i), i);
-        }
-        return res;
     }
+    */
+
+
+    // Sliding window using map
+    /*
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int begin = 0, end = 0, counter = 0, d = 0;
+
+        while (end < s.length()) {
+            // > 0 means repeating character
+            // if(map[s.charAt(end++)]-- > 0) counter++;
+            char c = s.charAt(end++);
+            map.put(c, 1 + map.getOrDefault(c, 0));
+            if(map.get(c) > 1) counter++;
+
+            while (counter > 0) {
+                //if (map[s.charAt(begin++)]-- > 1) counter--;
+                char charTemp = s.charAt(begin++);
+                if (map.get(charTemp) > 1) counter--;
+                map.put(charTemp, -1 + map.get(charTemp));
+            }
+            d = Math.max(d, end - begin);
+        }
+        return d;
+    }
+    */
 }
