cf8925d919c87f0f67823bf3d2a3254dcf1629f2
wstcliyu <wstcliyu@gmail.com>
2019-02-05 00:14:48 -0500

    62 DP

---
+++ Unique_62.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Unique_62 {
+    // DP
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        Arrays.fill(dp[0], 1);
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j < n; j++)
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
+        return dp[m - 1][n - 1];
+    }
+}
