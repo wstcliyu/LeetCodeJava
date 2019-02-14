180e6ff7503ad3d8536dd0bb05541567beca1f36
wstcliyu <wstcliyu@gmail.com>
2019-02-14 09:38:57 -0500

    520 Character.isUpperCase(c)

--- DetectCapital_520.java
+++ DetectCapital_520.java
@@ -4,15 +4,18 @@ public class DetectCapital_520 {
         int countCapital = 0;
         for (int i=0; i<n; i++) {
             char c = word.charAt(i);
-            if (c-'A'>=0 && c-'A'<26)
-                countCapital++;
+            // Character.isUpperCase(c)
+            if (c-'A'>=0 && c-'A'<26) countCapital++;
         }
+        /*
         if (countCapital == n || countCapital == 0)
             return true;
         char c = word.charAt(0);
         if (countCapital == 1 && c-'A'>=0 && c-'A'<26)
             return true;
         return false;
+        */
+        return countCapital == n || countCapital == 0 || countCapital == 1 && Characer.isUpperCase(word.charAt(0));
     }
     public static void main (String args[]) {
         String testWord = "FlaG";
