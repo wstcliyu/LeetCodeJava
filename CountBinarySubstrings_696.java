5cc4ad1582d09d9fe9af0baa86b180f0510a6a10
wstcliyu <wstcliyu@gmail.com>
2019-02-14 09:40:48 -0500

    696 add standard solution

--- CountBinarySubstrings_696.java
+++ CountBinarySubstrings_696.java
@@ -1,4 +1,22 @@
 public class CountBinarySubstrings_696 {
+    // Standard solution #2: Linear Scan
+    public int countBinarySubstrings(String s) {
+        int ans = 0, prev = 0, cur = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i-1) != s.charAt(i)) {
+                ans += Math.min(prev, cur);
+                prev = cur;
+                cur = 1;
+            } else {
+                cur++;
+            }
+        }
+        return ans + Math.min(prev, cur);
+    }
+
+
+    // My first solution (similar with standard solution #2)
+    /*
     public static int countBinarySubstrings(String s) {
         int count = 0;
         int consecutive = 0;
@@ -16,6 +34,8 @@ public class CountBinarySubstrings_696 {
         }
         return count;
     }
+    */
+
     public static void main (String args[]) {
         String testS = "00110011";
         System.out.println(countBinarySubstrings(testS));
