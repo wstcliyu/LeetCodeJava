1d178b685505c4e70d4d6e3ecf1ce77d5f04a1f2
wstcliyu <wstcliyu@gmail.com>
2019-02-14 19:17:54 -0500

    606 Construct string from tree

--- Construct_606.java
+++ Construct_606.java
@@ -12,19 +12,15 @@ public class Construct_606 {
 
     // https://leetcode.com/problems/construct-string-from-binary-tree/solution/
 
-
-    // Standard Solution Approach #1 Using Recursion
+    // Standard Solution Approach #1: Using Recursion
     // Time: O(n). The preorder traversal is done over the nn nodes of the given Binary Tree.
     // Space: O(n). The depth of the recursion tree can go upto nn in case of a skewed tree.
     /*
     public String tree2str(TreeNode t) {
-        if(t==null)
-            return "";
-        if(t.left==null && t.right==null)
-            return t.val+"";
-        if(t.right==null)
-            return t.val+"("+tree2str(t.left)+")";
-        return t.val+"("+tree2str(t.left)+")("+tree2str(t.right)+")";
+        if(t == null) return "";
+        if(t.left == null && t.right == null) return t.val+"";
+        if(t.right==null) return t.val + "("+tree2str(t.left)+")";
+        return t.val+"(" + tree2str(t.left) + ")(" + tree2str(t.right) + ")";
     }
     */
 
@@ -34,11 +30,10 @@ public class Construct_606 {
     // Space: O(n)
     /*
     public String tree2str(TreeNode t) {
-        if (t == null)
-            return "";
-        Stack< TreeNode > stack = new Stack < > ();
+        if (t == null) return "";
+        Stack<TreeNode> stack = new Stack < > ();
         stack.push(t);
-        Set< TreeNode > visited = new HashSet < > ();
+        Set<TreeNode> visited = new HashSet < > ();
         StringBuilder s = new StringBuilder();
         while (!stack.isEmpty()) {
             t = stack.peek();
