7c5d0edb5506a691fcbef3d1c527dce37a200a14
wstcliyu <wstcliyu@gmail.com>
2019-02-17 14:48:58 -0500

    125 Palindrome using two pointers template

--- Valid_125.java
+++ Valid_125.java
@@ -1,28 +1,26 @@
 public class Valid_125 {
+    // Palindrome using two pointers template
+    // Updated my solutinon
     public static boolean isPalindrome(String s) {
-        // Good practice for two pointers
+        int left = 0, right = s.length()-1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+
+
+    /*
+    public static boolean isPalindrome(String s) {
         int left = 0, right = s.length()-1;
         while (left < right) {
             char cLeft = s.charAt(left);
             char cRight = s.charAt(right);
-            /* My solution
-            while (left < right) {
-                if (Character.isLetterOrDigit(cLeft))
-                    break;
-                else
-                    left++;
-            }
-            while (left < right) {
-                if (Character.isLetterOrDigit(cRight))
-                    break;
-                else
-                    right--;
-            }
-            if (Character.toLowerCase(cLeft) != Character.toLowerCase(cRight))
-                return false;
-            left++;
-            right--;
-            */
             if (!Character.isLetterOrDigit(cLeft))
                 left++;
             else if (!Character.isLetterOrDigit(cRight))
@@ -36,6 +34,8 @@ public class Valid_125 {
         }
         return true;
     }
+    */
+
     public static void main (String[] args) {
         String testS = "race a car";
         System.out.println(isPalindrome(testS));
