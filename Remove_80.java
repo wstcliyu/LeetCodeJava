64d1c68679c7a8ebd0a55fe7ee6557b4c8d0636d
wstcliyu <wstcliyu@gmail.com>
2019-02-18 17:37:58 -0500

    80 write read anchor

--- Remove_80.java
+++ Remove_80.java
@@ -1,6 +1,21 @@
 import java.util.Arrays;
 
 public class Remove_80 {
+    // My updated solution using write, read, anchor
+    public int removeDuplicates(int[] nums) {
+        int write = 0, read = 0, anchor = 0;
+        while (read < nums.length) {
+            while (read < nums.length && nums[read] == nums[anchor])
+                read++;
+            int count = Math.min(2, read - anchor);
+            for (int i = 0; i < count; i++) nums[write++] = nums[anchor];
+            anchor = read;
+        }
+        return write;
+    }
+
+
+    /*
     public static int removeDuplicates(int[] nums) {
         int res = 1, count = 1, anchor = 1;
         for (int i=1; i<nums.length; i++) {
@@ -17,6 +32,7 @@ public class Remove_80 {
         }
         return res;
     }
+    */
     public static void main (String[] args) {
         int[] testNums = {1,1,1,2,2,3};
         System.out.println(removeDuplicates(testNums));
