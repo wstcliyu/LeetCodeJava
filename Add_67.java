d6e2e0f8a0ba13f56cfd0ef0b1407e201fe4cc68
wstcliyu <wstcliyu@gmail.com>
2019-02-20 14:57:29 -0500

    67 Add binary

--- Add_67.java
+++ Add_67.java
@@ -1,19 +1,15 @@
 public class AddBinary_67 {
     public static String addBinary(String a, String b) {
-        int i = a.length()-1;
-        int j = b.length()-1;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
         int sum = 0;
         StringBuilder result = new StringBuilder();
-        while (i>=0 || j>=0) {
-            if ( i>=0 )
-                sum += a.charAt(i--) - '0';
-            if ( j>=0 )
-                sum += b.charAt(j--) - '0';
+        while (i >= 0 || j >= 0 || sum > 0) {
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
             result.append(sum%2);
             sum = sum/2;
         }
-        if (sum != 0)
-            result.append(sum);
         return result.reverse().toString();
     }
     public static void main(String[] args) {
