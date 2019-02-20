a331da0c106825bc3bccc22bc9de2382f6516c53
wstcliyu <wstcliyu@gmail.com>
2019-02-20 13:07:21 -0500

    819 Most common word

--- Most_819.java
+++ Most_819.java
@@ -9,6 +9,10 @@ public class Most_819 {
         HashSet<String> ban = new HashSet<>(Arrays.asList(banned));
         // String[] words = paragraph.replaceAll("\\p{Punct}", " ").toLowerCase().split("\\s+");
         String[] words = paragraph.replaceAll("\\pP", "").toLowerCase().split("\\s+");
+
+        // String[] words = p.replaceAll("\\W+" , " ").toLowerCase().split("\\s+");
+        // \w is a word character, \W is a non-wor character
+
         HashMap<String, Integer> count = new HashMap<>();
         for (String word : words)
             if (!ban.contains(word))
@@ -21,4 +25,34 @@ public class Most_819 {
         String testResult = mostCommonWord(testParagraph, testBanned);
         System.out.println(testResult);
     }
+
+
+    // Good submission without using built-in string methods
+    /*
+    public String mostCommonWord(String paragraph, String[] banned) {
+        paragraph += ".";
+        Set<String> banset = new HashSet();
+        for (String word: banned) banset.add(word);
+        Map<String, Integer> count = new HashMap();
+        String ans = "";
+        int ansfreq = 0;
+        StringBuilder word = new StringBuilder();
+        for (char c: paragraph.toCharArray()) {
+            if (Character.isLetter(c)) {
+                word.append(Character.toLowerCase(c));
+            } else if (word.length() > 0) {
+                String finalword = word.toString();
+                if (!banset.contains(finalword)) {
+                    count.put(finalword, count.getOrDefault(finalword, 0) + 1);
+                    if (count.get(finalword) > ansfreq) {
+                        ans = finalword;
+                        ansfreq = count.get(finalword);
+                    }
+                }
+                word = new StringBuilder();
+            }
+        }
+        return ans;
+    }
+    */
 }
