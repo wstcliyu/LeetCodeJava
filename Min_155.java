2ccc95d631fc97147b0c0f176a36345b3a5def23
wstcliyu <wstcliyu@gmail.com>
2019-02-21 10:17:51 -0500

    155 Min stack

--- Min_155.java
+++ Min_155.java
@@ -5,6 +5,48 @@ public class Min_155 {
     // Most voted solution uses only one stack
     // https://leetcode.com/problems/min-stack/discuss/49031/Share-my-Java-solution-with-ONLY-ONE-stack
 
+    long min;
+    Stack<Long> stack;
+
+    public Min_155(){
+        stack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        if (stack.isEmpty()){
+            stack.push(0L);
+            min = x;
+        } else {
+            stack.push(x - min);//Could be negative if min value needs to change
+            if (x < min) min = x;
+        }
+    }
+
+    public void pop() {
+        if (stack.isEmpty()) return;
+        
+        long pop = stack.pop();
+        
+        if (pop < 0)  
+            min = min - pop;//If negative, increase the min value
+        
+    }
+
+    public int top() {
+        long top = stack.peek();
+        if (top > 0){
+            return (int)(top+min);
+        } else {
+           return (int)(min);
+        }
+    }
+
+    public int getMin() {
+        return (int)min;
+    }
+
+
+    /*
     Deque<Integer> st;
     Deque<Integer> m;
 
@@ -33,4 +75,5 @@ public class Min_155 {
     public int getMin() {
         return m.peek();
     }
+    */
 }
