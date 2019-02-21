72fe8909c5a3be8b80b0a488246335b3c05fdaf0
wstcliyu <wstcliyu@gmail.com>
2019-02-21 11:12:08 -0500

    230 Kth Smallest Element in a BST

--- Kth_230.java
+++ Kth_230.java
@@ -32,11 +32,11 @@ public class Kth_230 {
     // Binary Search
     public int kthSmallest(TreeNode root, int k) {
         int count = countNodes(root.left);
-        if (k <= count)
+        if (count >= k)
             return kthSmallest(root.left, k);
-        else if (k > count + 1)
+        else if (count < k-1)
             return kthSmallest(root.right, k-1-count);
-        return root.val;
+        return root.val; // count == k-1
     }
 
     public int countNodes(TreeNode root) {
