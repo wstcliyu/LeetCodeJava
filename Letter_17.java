9cddb3a698b609a935b6b37c889bb7c42b291104
wstcliyu <wstcliyu@gmail.com>
2019-02-21 20:18:27 -0500

    17 Backtrack

--- Letter_17.java
+++ Letter_17.java
@@ -5,9 +5,11 @@ public class Letter_17 {
     // Most voted solution with FIFO queue
     public List<String> letterCombinations(String digits) {
 		LinkedList<String> ans = new LinkedList<String>();
-		if(digits.isEmpty()) return ans;
+		if (digits.isEmpty()) return ans;
 		String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 		ans.add("");
+
+        // Online solution
 		/*
 		for(int i =0; i<digits.length();i++){
 			int x = Character.getNumericValue(digits.charAt(i));
@@ -18,55 +20,38 @@ public class Letter_17 {
 			}
 		}
 		*/
-		while(ans.peek().length()!=digits.length()){
-			String remove = ans.remove();
-			String map = mapping[digits.charAt(remove.length())-'0'];
+
+        // My version
+		while (ans.getFirst().length() != digits.length()) {
+			String peek = ans.removeFirst();
+			String map = mapping[digits.charAt(peek.length()) - '0'];
 			for(char c: map.toCharArray())
-				ans.addLast(remove+c);
+				ans.addLast(peek+c);
 		}
 		return ans;
 	}
 
+
 	// My solution with backtracking
     /*
+    String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
     public List<String> letterCombinations(String digits) {
         List<String> res = new LinkedList<>();
         if (digits == null || digits.length() == 0)
             return res;
-        backtrack(res, new StringBuilder(), digits, 0);
+        backtrack(res, new StringBuilder(), digits);
         return res;
     }
-    private void backtrack (List<String> ls, StringBuilder sb, String digits, int start) {
-        if (start == digits.length())
+
+    private void backtrack (List<String> ls, StringBuilder sb, String digits) {
+        if (sb.length() == digits.length())
             ls.add(sb.toString());
         else {
-            int d = digits.charAt(start) - '2';
-            int x = 0, y = 0;
-            switch(d) {
-                case 0:
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                    x = 3 * d;
-                    y = 3;
-                    break;
-                case 5:
-                    x = 3 * d;
-                    y = 4;
-                    break;
-                case 6:
-                    x = 3 * d + 1;
-                    y = 3;
-                    break;
-                case 7:
-                    x = 3 * d + 1;
-                    y = 4;
-                    break;
-            }
-            for (int i=0; i<y; i++) {
-                sb.append((char)('a'+x+i));
-                backtrack(ls, sb, digits, start+1);
+            String map = mapping[digits.charAt(sb.length())-'0'];
+            for (char c: map.toCharArray()) {
+                sb.append(c);
+                backtrack(ls, sb, digits);
                 sb.deleteCharAt(sb.length()-1);
             }
         }
