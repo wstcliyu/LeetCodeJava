34089bec659e3be5ba81f3c03465d1a5af12e0c8
wstcliyu <wstcliyu@gmail.com>
2019-02-21 23:31:05 -0500

    347 Top K Frequent Elements

--- Top_347.java
+++ Top_347.java
@@ -2,56 +2,55 @@ import java.util.*;
 
 public class Top_347 {
 
-    // Standard solution using Heap
+    // Standard solution using min heap
     // Time: O(Nlog(k)). The complexity of Counter method is O(N). To build a heap and output list takes O(Nlog(k)). 
     // Hence the overall complexity of the algorithm is O(N+Nlog(k))=O(Nlog(k))
     // Space : O(N) to store the hash map
     public List<Integer> topKFrequent(int[] nums, int k) {
-        // build hash map : character and how often it appears
         HashMap<Integer, Integer> count = new HashMap();
-        for (int n: nums) 
-            count.put(n, count.getOrDefault(n, 0) + 1);
+        for (int n: nums) count.put(n, count.getOrDefault(n, 0) + 1);
 
         // init heap 'the less frequent element first'
-        PriorityQueue<Integer> heap =
+        PriorityQueue<Integer> minHeap =
                 new PriorityQueue<Integer>((n1, n2) -> count.get(n1) - count.get(n2));
+        // My version
+        // PriorityQueue<Integer> minHeap =
+        //        new PriorityQueue<Integer>(k+1, Comparator.comparing((Integer i) -> count.get(i)));
+
 
         // keep k top frequent elements in the heap
         for (int n: count.keySet()) {
-            heap.add(n);
-            if (heap.size() > k)
-                heap.poll();
+            minHeap.add(n);
+            if (minHeap.size() > k) minHeap.poll();
         }
 
         // build output list
+        // My version: return new ArrayList<>(minHeap);
         List<Integer> top_k = new LinkedList();
-        while (!heap.isEmpty())
-            top_k.add(heap.poll());
-
+        while (!minHeap.isEmpty()) top_k.add(minHeap.poll());
         Collections.reverse(top_k);
         return top_k;
     }
 
-    // My solution using priority queue
+
+    // My first solution using max heap
     /*
     public List<Integer> topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> count = new HashMap();
-        for (int n: nums) 
-            count.put(n, count.getOrDefault(n, 0) + 1);
+        for (int n: nums) count.put(n, count.getOrDefault(n, 0) + 1);
 
-        PriorityQueue<Integer> heap =
+        PriorityQueue<Integer> maxHeap =
                 new PriorityQueue<Integer>((n1, n2) -> count.get(n2) - count.get(n1));
-        heap.addAll(count.keySet());
+        maxHeap.addAll(count.keySet());
 
         List<Integer> top_k = new LinkedList();
-        for (int i = 0; i < k; i++)
-            top_k.add(heap.poll());
+        for (int i = 0; i < k; i++) top_k.add(maxHeap.poll());
         return top_k;
     }
     */
 
 
-    // Most voted solution
+    // Most voted solution using bucket sort
     // Time: O(n)
     /*
     public List<Integer> topKFrequent(int[] nums, int k) {
@@ -73,4 +72,22 @@ public class Top_347 {
         return res;
     }
     */
+
+
+    // Using TreeMap whose <key, value> = <frequency, num>
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) count.put(num, 1 + count.getOrDefault(num, 0));
+
+        TreeMap<Integer, List<Integer>> freqMap = new TreeMap<>();
+        for (int num : count.keySet()) {
+            int freq = count.get(num);
+            freqMap.putIfAbsent(freq, new ArrayList<>());
+            freqMap.get(freq).add(num);
+        }
+
+        List<Integer> res = new ArrayList<>();
+        while (res.size() < k) res.addAll(freqMap.pollLastEntry().getValue());
+        return res;
+    }
 }
