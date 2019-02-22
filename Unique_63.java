62b12f133cf255c2547be667cf137bd799bd792c
wstcliyu <wstcliyu@gmail.com>
2019-02-21 21:05:49 -0500

    63 Unique paths II

--- Unique_63.java
+++ Unique_63.java
@@ -1,5 +1,5 @@
 public class Unique_63 {
-    // DP
+    // 2D DP
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
@@ -17,4 +17,23 @@ public class Unique_63 {
         }
         return dp[m - 1][n - 1];
     }
+
+
+    // 1D DP
+    /*
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    int width = obstacleGrid[0].length;
+    int[] dp = new int[width];
+    dp[0] = 1;
+    for (int[] row : obstacleGrid) {
+        for (int j = 0; j < width; j++) {
+            if (row[j] == 1)
+                dp[j] = 0;
+            else if (j > 0)
+                dp[j] += dp[j - 1];
+        }
+    }
+    return dp[width - 1];
+}
+    */
 }
