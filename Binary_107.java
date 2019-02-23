a07f65539552ecf7e8ec9f47262f896f16c5965c
wstcliyu <wstcliyu@gmail.com>
2019-02-22 19:33:34 -0500

    107 Binary Tree Level Order Traversal II

--- Binary_107.java
+++ Binary_107.java
@@ -8,16 +8,17 @@ class Binary_107 {
 	public List<List<Integer>> levelOrderBottom(TreeNode root) {
         List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
         levelMaker(wrapList, root, 0);
+        // We can also copy the code in Binary_102, and then Collections.reverse(wrapList);
         return wrapList;
     }
     
     public void levelMaker(List<List<Integer>> list, TreeNode root, int level) {
         if(root == null) return;
-        if(level >= list.size()) {
-            list.add(0, new LinkedList<Integer>());
-        }
+        if(level >= list.size()) list.add(0, new LinkedList<Integer>());
+        list.get(list.size()-level-1).add(root.val);
         levelMaker(list, root.left, level+1);
         levelMaker(list, root.right, level+1);
-        list.get(list.size()-level-1).add(root.val);
+        // list.get(list.size()-level-1).add(root.val);
+        // Either pre-order or post-order is OK here.
     }
 }
