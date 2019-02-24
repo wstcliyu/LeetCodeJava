a2deee86424ee5ac0cdd318ef82ed0a4317588bb
wstcliyu <wstcliyu@gmail.com>
2019-02-23 22:01:58 -0500

    252 Meeting Rooms

---
+++ Meeting_252.java
@@ -0,0 +1,14 @@
+import java.util.Comparator;
+
+public class Meeting_252 {
+	class Interval {
+		int start, end;
+	}
+	public boolean canAttendMeetings(Interval[] intervals) {
+        Arrays.sort(intervals, Comparator.comparing((Interval i) -> i.start));
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i].end > intervals[i+1].start) return false;
+        }
+        return true;
+    }
+}
