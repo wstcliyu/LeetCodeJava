e68376213735857c1ca8029f09af063e039e4780
wstcliyu <wstcliyu@gmail.com>
2019-02-24 20:32:48 -0500

    280 Wiggle Sort

---
+++ Wiggle_280.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Wiggle_280 {
+    // Standard solution #2: One-pass swap
+    public void wiggleSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if ((i % 2 == 0) == (nums[i] > nums[i + 1])) {
+                swap(nums, i, i + 1);
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+
+    // Standard solution #1: Sort
+    /*
+    public void wiggleSort(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length - 1; i += 2) {
+            swap(nums, i, i + 1);
+        }
+    }
+    */
+}
