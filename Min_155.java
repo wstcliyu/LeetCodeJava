7f54de5d45b95404623b34fbacc3107bd93af5ac
wstcliyu <wstcliyu@gmail.com>
2019-02-26 16:09:44 -0500

    155 Min Stack

--- Min_155.java
+++ Min_155.java
@@ -1,13 +1,48 @@
 import java.util.*;
 
 public class Min_155 {
-    // Most voted solution uses only one stack
-    // https://leetcode.com/problems/min-stack/discuss/49031/Share-my-Java-solution-with-ONLY-ONE-stack
+    // Most voted solution using one stack
 
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack;
+
+    public Min_155(){
+        stack = new Stack<>();
+    }
+
+    public void push(int x) {
+        // only push the old minimum value when the current 
+        // minimum value changes after pushing the new value x
+        if (x <= min){          
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        // if pop operation could result in the changing of the current minimum value, 
+        // pop twice and change the current minimum value to the last minimum value.
+        if (stack.pop() == min) min = stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+
+
+    // Second voted solution uses only one stack
+    // https://leetcode.com/problems/min-stack/discuss/49031/Share-my-Java-solution-with-ONLY-ONE-stack
+    /*
     long min;
     Stack<Long> stack;
 
-    public Min_155(){
+    public MinStack(){
         stack = new Stack<>();
     }
     
@@ -26,15 +61,14 @@ public class Min_155 {
         
         long pop = stack.pop();
         
-        if (pop < 0)  
-            min = min - pop;//If negative, increase the min value
+        if (pop < 0)  min = min - pop;//If negative, increase the min value
         
     }
 
     public int top() {
         long top = stack.peek();
         if (top > 0){
-            return (int)(top+min);
+            return (int)(top + min);
         } else {
            return (int)(min);
         }
@@ -43,8 +77,11 @@ public class Min_155 {
     public int getMin() {
         return (int)min;
     }
+    */
 
 
+
+    // Using two stacks
     /*
     Deque<Integer> st;
     Deque<Integer> m;
