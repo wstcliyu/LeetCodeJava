e2833b9eefb726084c7578cdf3c1141a61a6d6ed
wstcliyu <wstcliyu@gmail.com>
2019-02-27 17:51:40 -0500

    My Max Heap

--- MaxHeap.java
+++ MaxHeap.java
@@ -15,11 +15,17 @@ public class MaxHeap {
         return size == 0;
     }
 
+    public int getSize() {
+        return size;
+    }
+
     public int peek() {
         return ls.get(1);
     }
 
-    public int poll() {
+    public int poll() throws Exception {
+        if (isEmpty())
+            throw new Exception("Heap is empty! Cannot poll!");
         int max = peek();
         ls.set(1, ls.get(size));
         ls.remove(size--);
@@ -56,15 +62,18 @@ public class MaxHeap {
 
     public static void main(String[] args) {
         MaxHeap mh = new MaxHeap();
-        for (int i = -5; i < 0; i++) mh.add(i);
+        for (int i = 5; i < 8; i++) mh.add(i);
         System.out.println(mh.isEmpty());
-        System.out.println(mh.poll());
-        System.out.println(mh.poll());
-        System.out.println(mh.poll());
-        mh.add(1);
-        System.out.println(mh.poll());
-        System.out.println(mh.poll());
-        System.out.println(mh.poll());
-        System.out.println(mh.isEmpty());
+        try {
+            System.out.println(mh.isEmpty());
+            System.out.println(mh.poll());
+            System.out.println(mh.poll());
+            System.out.println(mh.poll());
+            System.out.println(mh.getSize());
+            System.out.println(mh.isEmpty());
+            System.out.println(mh.poll());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
