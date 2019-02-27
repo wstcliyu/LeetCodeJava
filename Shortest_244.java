dbd82cb11a65e41ae442b959cea8c747d96543c7
wstcliyu <wstcliyu@gmail.com>
2019-02-27 10:52:09 -0500

    244 Shortest Word Distance II

---
+++ Shortest_244.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Shortest_244 {
+
+    HashMap<String, ArrayList<Integer>> map = new HashMap<>();
+
+    public Shortest_244(String[] words) {
+        for(int i = 0; i < words.length; i++){
+            map.putIfAbsent(words[i], new ArrayList<>());
+            map.get(words[i]).add(i);
+        }
+    }
+
+    public int shortest(String word1, String word2) {
+        ArrayList<Integer> l1 = map.get(word1);
+        ArrayList<Integer> l2 = map.get(word2);
+
+        int result = Integer.MAX_VALUE;
+        int i = 0, j = 0;
+        while(i < l1.size() && j < l2.size()) {
+            result = Math.min(result, Math.abs(l1.get(i) - l2.get(j)));
+            if (l1.get(i) < l2.get(j)) i++;
+            else j++;
+        }
+
+        return result;
+    }
+
+}
