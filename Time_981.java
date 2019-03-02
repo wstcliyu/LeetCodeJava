52c152c5160fe0ab9d14fb03e89ffbce6eed71a4
wstcliyu <wstcliyu@gmail.com>
2019-03-02 12:47:34 -0500

    981 Time Based Key-Value Store

---
+++ Time_981.java
@@ -0,0 +1,25 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class Time_981 {
+	Map<String, TreeMap<Integer, String>> M;
+
+    public TimeMap() {
+        M = new HashMap();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        M.putIfAbsent(key, new TreeMap());
+        M.get(key).put(timestamp, value);
+    }
+
+    public String get(String key, int timestamp) {
+        if (!M.containsKey(key)) return "";
+        TreeMap<Integer, String> tree = M.get(key);
+        // Integer t = tree.floorKey(timestamp);
+        // return t != null ? tree.get(t) : "";
+        Map.Entry<Integer, String> entry = tree.floorEntry(timestamp);
+        return entry != null ? entry.getValue() : "";
+    }
+}
