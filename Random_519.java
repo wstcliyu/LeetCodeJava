f543d070d100c8a5c6dbddca254c42760db1d22b
wstcliyu <wstcliyu@gmail.com>
2019-03-03 20:30:11 -0500

    519 Random Flip Matrix

--- Random_519.java
+++ Random_519.java
@@ -3,6 +3,9 @@ import java.util.Map;
 import java.util.Random;
 
 public class Random_519 {
+
+    // Similar with Random_710
+
     int R, C, total;
     Map<Integer, Integer> map;
     Random rand;
@@ -19,7 +22,10 @@ public class Random_519 {
         int i = rand.nextInt(total--);
         int res = map.getOrDefault(i, i);
         map.put(i, map.getOrDefault(total, total));
-        map.put(total, res); // Adding this line so that we don't need to clear the map for reset because map.clear() takes O(n)
+        map.put(total, res);
+        // Adding this line (Line 22) so that we don't need to clear the map for reset
+        // because map.clear() takes O(n) time
+        // Line 20~22 actually swap the stored values of key i and total
         return new int[]{res / C, res % C};
     }
 
