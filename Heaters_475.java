d00db17b6ea5cc0e66fd31851e7948056d119ee0
wstcliyu <wstcliyu@gmail.com>
2019-03-03 22:29:41 -0500

    475 Heaters

--- Heaters_475.java
+++ Heaters_475.java
@@ -1,30 +1,33 @@
 import java.util.Arrays;
 
 public class Heaters_475 {
+    // Sort two arrays, then two pointers
+    /*
     public static int findRadius(int[] houses, int[] heaters) {
-        /* sort two arrays, then two pointers
         Arrays.sort(houses);
         Arrays.sort(heaters);
-        int i = 0, j = 0, res = 0;
-        while (i < houses.length) {
+        int j = 0, res = 0;
+        for (int i = 0; i < houses.length; i++) {
             while (j < heaters.length - 1
-                && Math.abs(heaters[j + 1] - houses[i]) <= Math.abs(heaters[j] - houses[i])) {
+                && Math.abs(heaters[j] - houses[i]) >= Math.abs(heaters[j+1] - houses[i])) {
                 j++;
             }
             res = Math.max(res, Math.abs(heaters[j] - houses[i]));
-            i++;
         }
         return res;
-        */
-        // sort one array, then use Arrays.binarySearch(int[] a, int key)
+    }
+    */
+        
+
+    // Sort one array, then use Arrays.binarySearch(int[] a, int key)
+    public static int findRadius(int[] houses, int[] heaters) {
         Arrays.sort(heaters);
         int res = 0;
-        for (int house:houses) {
-            int index = Arrays.binarySearch(heaters, house);
-            if (index < 0)
-                index = -index-1;
-            int dist1 = index>=1 ? house - heaters[index-1] : Integer.MAX_VALUE;
-            int dist2 = index<heaters.length ? heaters[index] - house : Integer.MAX_VALUE;
+        for (int house : houses) {
+            int p = Arrays.binarySearch(heaters, house);
+            if (p < 0) p = - p - 1;
+            int dist1 = (p >= 1) ? house - heaters[p-1] : Integer.MAX_VALUE;
+            int dist2 = (p < heaters.length) ? heaters[p] - house : Integer.MAX_VALUE;
             res = Math.max(res, Math.min(dist1, dist2));
         }
         return res;
