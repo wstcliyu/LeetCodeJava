3b7fd40dde722bbd5df1b0d4ec7910a3664c2dfe
wstcliyu <wstcliyu@gmail.com>
2019-03-05 12:15:06 -0500

    1002 Find Common Characters

---
+++ Find_1002.java
@@ -0,0 +1,23 @@
+public class Find_1002 {
+	public List<String> commonChars(String[] A) {
+        int[] count = new int[26];
+        Arrays.fill(count, 100);
+        for (String s : A) updateCount(s, count);
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            char c = (char)('a' + i);
+            for (int j = 0; j < count[i]; j++)
+                res.add(c + "");
+        }
+        return res;
+    }
+    
+    
+    private void updateCount(String s, int[] count) {
+        int[] tmp = new int[26];
+        for (int i = 0; i < s.length(); i++)
+            tmp[s.charAt(i) - 'a']++;
+        for (int i = 0; i < 26; i++)
+            count[i] = Math.min(count[i], tmp[i]);
+    }
+}
