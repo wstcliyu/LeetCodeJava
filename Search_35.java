89061e071bef674bcea85db242453ed1c2aa8173
wstcliyu <wstcliyu@gmail.com>
2019-03-19 00:10:59 -0400

    35 Search Insert Position

--- Search_35.java
+++ Search_35.java
@@ -1,4 +1,22 @@
 public class Search_35 {
+    // My updated solution
+    /*
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] < target) 
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+    */
+
+
+
+
     public static int searchInsert(int[] nums, int target) {
         // int p = Arrays.binarySearch(nums, target);
         // return p < 0 ? - p - 1 : p;
