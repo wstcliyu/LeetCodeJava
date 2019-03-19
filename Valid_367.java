51c45a2496ddcbc1ce226bf018cf510f8a577a55
wstcliyu <wstcliyu@gmail.com>
2019-03-19 00:14:12 -0400

    367 Valid Perfect Square

--- Valid_367.java
+++ Valid_367.java
@@ -1,4 +1,21 @@
 public class Valid_367 {
+    // My updated solution
+    /*
+    public boolean isPerfectSquare(int num) {
+        int left = 1, right = num;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (mid < num/mid)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return num % left == 0 && left == num / left;
+    }
+    */
+
+
+
     public static boolean isPerfectSquare(int num) {
         int left = 1, right = num;
         while (left < right) {
