4f0bdd6350c3ebefc7e719f448999704f8208e28
wstcliyu <wstcliyu@gmail.com>
2019-03-19 00:20:28 -0400

    69 Sqrt(x)

--- Sqrt_69.java
+++ Sqrt_69.java
@@ -1,18 +1,15 @@
 public class Sqrt_69 {
-    public static int mySqrt(int x) {
-        if (x == 0)
-            return 0;
-        int left = 1, right = Integer.MAX_VALUE;
-        while (true) {
-            int mid = left + (right - left)/2;
-            if (mid > x/mid) {
-                right = mid - 1;
-            } else {
-                if (mid + 1 > x/(mid + 1))
-                    return mid;
-                left = mid + 1;
-            }
+    public int mySqrt(int x) {
+        if (x <= 1) return x;
+        int l = 1, r = x;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (mid > x / mid)
+                r = mid;
+            else
+                l = mid + 1;
         }
+        return r - 1;
     }
     public static void main (String args[]) {
         System.out.println(mySqrt(10));
