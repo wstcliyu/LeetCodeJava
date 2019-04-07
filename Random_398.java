21864e06a8019863bf0a25777fdfb465f11b2302
wstcliyu <wstcliyu@gmail.com>
2019-04-07 16:40:53 -0400

    398 Random Pick Index

---
+++ Random_398.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+class Random_398 {
+    // Most voted solution using reservoir sampling
+    // Similar with Linked_382
+    
+    int[] nums;
+    Random rand;
+    
+    public Solution(int[] nums) {
+        this.nums = nums;
+        rand = new Random();
+    }
+    
+    public int pick(int target) {
+        int res = -1, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != target) continue;
+            if (rand.nextInt(++count) == 0) res = i;
+        } 
+        return res;
+    }
+}
