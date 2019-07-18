dca2f8ea52626238eadb7e12360456b5d159044d
Yu Li <yli5@yli5-a01.vmware.com>
2019-07-18 11:47:44 -0700

    270 Closest Binary Search Tree Value

---
+++ Closest_270.java
@@ -0,0 +1,42 @@
+class Closest_270 {
+    public int closestValue(TreeNode root, double target) {
+        int res = root.val;
+        while (root != null) {
+            int val = root.val;
+            res = Math.abs(val - target) < Math.abs(res - target) ? val : res;
+            root = target < val ? root.left : root.right;
+        }
+        return res;
+    }
+
+
+
+    // My first solution
+    /*
+    public int closestValue(TreeNode root, double target) {
+        int prev = Integer.MIN_VALUE;
+        int post = Integer.MAX_VALUE;
+        TreeNode node = root;
+        while (node != null) {
+            if (target > node.val) {
+                prev = node.val;
+                node = node.right;
+            } else if (target < node.val) {
+                post = node.val;
+                node = node.left;
+            } else {
+                return node.val;
+            }
+        }
+        int res = 0;
+        if (prev != Integer.MIN_VALUE)
+            res = prev;
+        else
+            return post;
+        if (post != Integer.MAX_VALUE && post - target < target - prev)
+            res = post;
+
+        return res;
+    }
+    */
+}
