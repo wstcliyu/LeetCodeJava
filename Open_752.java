5a8aa4551266183075e62e1e090c6ce877ac6abc
wstcliyu <wstcliyu@gmail.com>
2019-07-21 17:50:53 -0700

    752 Open the Lock

--- Open_752.java
+++ Open_752.java
@@ -1,7 +1,51 @@
 import java.util.*;
 
 class Open_752 {
+    // Bi-directional BFS template (also refer to Word_127 for biBFS template)
+    // I rewrite most voted solution
+    public int openLock(String[] deadends, String target) {
+        Set<String> deads = new HashSet<>(Arrays.asList(deadends));
+        if (deads.contains("0000")) return -1;
+        if (target.equals("0000")) return 0;
+
+        Set<String> beginSet = new HashSet<>(Arrays.asList("0000"));
+        Set<String> endSet = new HashSet<>(Arrays.asList(target));
+        return biBFS(beginSet, endSet, deads, 1);
+    }
+    
+    private int biBFS(Set<String> beginSet, Set<String> endSet, Set<String> deads, int step) {
+        if (beginSet.size() == 0) return -1;
+        if (beginSet.size() > endSet.size()) return biBFS(endSet, beginSet, deads, step);
+
+        deads.addAll(beginSet);
+        deads.addAll(endSet);
+        
+        Set<String> nextSet = new HashSet<>();
+        
+        for (String s : beginSet) {
+            for(int i = 0; i < 4; i ++) {
+                char c = s.charAt(i);
+                String s1 = s.substring(0, i) + (c == '9' ? 0 : c - '0' + 1) + s.substring(i + 1);
+                String s2 = s.substring(0, i) + (c == '0' ? 9 : c - '0' - 1) + s.substring(i + 1);
+                if (endSet.contains(s1) || endSet.contains(s2)) return step;
+                if (!deads.contains(s1)) {
+                    // deads.add(s1);
+                    nextSet.add(s1);
+                }
+                if (!deads.contains(s2)) {
+                    // deads.add(s2);
+                    nextSet.add(s2);
+                }
+            }
+        }
+        
+        return biBFS(nextSet, endSet, deads, step + 1);
+    }
+
+
+
     // Most voted solution using BFS (from both ends to middle)
+    /*
     public int openLock(String[] deadends, String target) {
         Set<String> begin = new HashSet<>();
         Set<String> end = new HashSet<>();
@@ -37,7 +81,7 @@ class Open_752 {
         }
         return -1;
     }
-
+    */
 
 
 
