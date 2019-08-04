aa25cb096a7a38c9ce6b4a514839758791f70015
wstcliyu <wstcliyu@gmail.com>
2019-08-04 09:12:45 -0700

    314 Binary Tree Vertical Order Traversal

--- Binary_314.java
+++ Binary_314.java
@@ -3,6 +3,7 @@ import java.util.*;
 import javafx.util.Pair;
 
 public class Binary_314 {
+    // Most voted solution #1
     public List<List<Integer>> verticalOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) return res;
@@ -29,4 +30,58 @@ public class Binary_314 {
             res.add(map.get(i));
         return res;
     }
+
+
+
+    // Most voted solution #2
+    /*
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        List<List<Integer>> cols = new ArrayList<>();
+        if (root == null) {
+            return cols;
+        }
+        
+        int[] range = new int[] {0, 0};
+        getRange(root, range, 0);
+        
+        for (int i = range[0]; i <= range[1]; i++) {
+            cols.add(new ArrayList<Integer>());
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<Integer> colQueue = new LinkedList<>();
+        
+        queue.add(root);
+        colQueue.add(-range[0]);
+        
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            int col = colQueue.poll();
+            
+            cols.get(col).add(node.val);
+            
+            if (node.left != null) {
+                queue.add(node.left);   
+                colQueue.add(col - 1);
+            } 
+            if (node.right != null) {
+                queue.add(node.right);
+                colQueue.add(col + 1);
+            }
+        }
+        
+        return cols;
+    }
+
+    public void getRange(TreeNode root, int[] range, int col) {
+        if (root == null) {
+            return;
+        }
+        range[0] = Math.min(range[0], col);
+        range[1] = Math.max(range[1], col);
+        
+        getRange(root.left, range, col - 1);
+        getRange(root.right, range, col + 1);
+    }
+    */
 }
