847f16de55bd6429983fe246244b3fe60508d403
wstcliyu <wstcliyu@gmail.com>
2019-10-01 18:59:56 -0400

    210 Course Schedule II

--- Course_210.java
+++ Course_210.java
@@ -41,38 +41,39 @@ public class Course_210 {
     private Map<Integer, List<Integer>> adj;
     private List<Integer> topOrder;
 
-    private void init(int numCourses) {
-        isCycle = false;
+    private void init(int numCourses, int[][] prerequisites) {
         adj = new HashMap<>();
+        for (int[] p : prerequisites) {
+            adj.computeIfAbsent(p[1], new ArrayList<>()).add(p[0]);
+        }
         topOrder = new ArrayList<>();
         color = new int[numCourses];
         Arrays.fill(color, WHITE);
+        isCycle = false;
     }
 
     private void dfs(int node) {
         if (isCycle) return;
         color[node] = GRAY;
         for (int i : adj.getOrDefault(node, new ArrayList<>())) {
-            if (color[i] == WHITE) dfs(i);
-            else if (color[i] == GRAY) {
+            if (color[i] == WHITE) {
+                dfs(i);
+            } else if (color[i] == GRAY) {
                 isCycle = true;
                 return;
             }
         }
-        color[node] = BLACK;
         topOrder.add(node);
+        color[node] = BLACK;
     }
 
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        init(numCourses);
-        for (int[] pair : prerequisites) {
-            adj.putIfAbsent(pair[1], new ArrayList<>());
-            adj.get(pair[1]).add(pair[0]);
-        }
-        for (int i = 0; i < numCourses && !isCycle; i++) {
-            if (color[i] == WHITE) dfs(i);
-        }
-        if (isCycle) return new int[0];
+        init(numCourses, prerequisites);
+        for (int i = 0; i < numCourses && !isCycle; i++)
+            if (color[i] == WHITE)
+                dfs(i);
+        if (isCycle)
+            return new int[0];
         int[] res = new int[numCourses];
         for (int i = 0; i < numCourses; i++)
             res[i] = topOrder.get(numCourses - 1 - i);
