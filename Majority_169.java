09e93245472aebbf31a912135a46f2bf7a09ea2e
YuLi <wstcliyu.gmail.com>
2019-11-11 00:07:08 -0500

    169 Majority Element

--- Majority_169.java
+++ Majority_169.java
@@ -4,43 +4,17 @@ import java.util.Random;
 public class Majority_169 {
     // Standard solution #3 (Sorting)
     // Time O(nlogn) Space O(1) or O(n)
+    /*
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
         return nums[nums.length/2];
     }
-
-
-    // Standard solution #4 (Randomization)
-    // Time O(infinity), Expected time is 2, Space O(1)
-    /*
-    private int randRange(Random rand, int min, int max) {
-        return rand.nextInt(max - min) + min;
-    }
-
-    private int countOccurences(int[] nums, int num) {
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == num)
-                count++;
-        }
-        return count;
-    }
-
-    public int majorityElement(int[] nums) {
-        Random rand = new Random();
-        int majorityCount = nums.length/2;
-        while (true) {
-            int candidate = nums[randRange(rand, 0, nums.length)];
-            if (countOccurences(nums, candidate) > majorityCount)
-                return candidate;
-        }
-    }
     */
+
 
 
     // Standard solution #6 (Boyer-Moore Voting Algorithm)
     // Time O(n) Space O(1)
-    /*
     public int majorityElement(int[] nums) {
         int count = 0;
         Integer candidate = null;
@@ -54,21 +28,15 @@ public class Majority_169 {
 
         return candidate;
     }
-    */
+
 
 
     // Standard solution #5 (Divide and Conquer)
     // Time O(nlogn) T(n) = 2T(n/2) + 2n
     // Space O(logn)
     /*
-    private int countInRange(int[] nums, int num, int lo, int hi) {
-        int count = 0;
-        for (int i = lo; i <= hi; i++) {
-            if (nums[i] == num) {
-                count++;
-            }
-        }
-        return count;
+    public int majorityElement(int[] nums) {
+        return majorityElementRec(nums, 0, nums.length-1);
     }
     
     private int majorityElementRec(int[] nums, int lo, int hi) {
@@ -95,21 +63,47 @@ public class Majority_169 {
         return leftCount > rightCount ? left : right;
     }
 
+    private int countInRange(int[] nums, int num, int lo, int hi) {
+        int count = 0;
+        for (int i = lo; i <= hi; i++) {
+            if (nums[i] == num) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public int majorityElement(int[] nums) {
         return majorityElementRec(nums, 0, nums.length-1);
     }
     */
 
+
+
+    // Standard solution #4 (Randomization)
+    // Time O(infinity), Expected time is 2, Space O(1)
     /*
-    public int majorityElement(int[] nums) {
+    private int randRange(Random rand, int min, int max) {
+        return rand.nextInt(max - min) + min;
+    }
+
+    private int countOccurences(int[] nums, int num) {
         int count = 0;
-        Integer candidate = null;
-        for (int num : nums) {
-            if (count == 0)
-                candidate = num;
-            count += (num == candidate) ? 1 : -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num)
+                count++;
         }
-        return candidate;
+        return count;
+    }
+
+    public int majorityElement(int[] nums) {
+        Random rand = new Random();
+        int majorityCount = nums.length/2;
+        while (true) {
+            int candidate = nums[randRange(rand, 0, nums.length)];
+            if (countOccurences(nums, candidate) > majorityCount)
+                return candidate;
+        }
     }
     */
 }
