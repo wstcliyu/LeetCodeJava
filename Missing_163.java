eca026b990b2fdd4d665f5195d988ac491a1a054
YuLi <wstcliyu.gmail.com>
2019-11-11 15:38:16 -0500

    163 Missing Ranges

--- Missing_163.java
+++ Missing_163.java
@@ -5,27 +5,34 @@ public class Missing_163 {
     // This problem has two corner cases (this solution just settles the second):
     // (1) The last element is Integer.MAX_VALUE. It causes next = a[i] + 1 overflow
     // (2) The array is empty but still we need to print lo and hi
-    public List<String> findMissingRanges(int[] a, int lo, int hi) {
+    public List<String> findMissingRanges(int[] nums, int lo, int hi) {
         List<String> res = new ArrayList<>();
+        if (nums.length == 0) {
+            res.add(getRange(lo, hi));
+            return res;
+        }
 
          // the next number we need to find
         int next = lo;
 
-        for (int i = 0; i < a.length; i++) {
+        for (int num : num) {
             // not within the range yet
-            if (a[i] < next) continue;
+            if (num < next) continue;
 
             // continue to find the next one
-            if (a[i] == next) {
+            if (num == next) {
+                if (num == Integer.MAX_VALUE) return res;
                 next++;
                 continue;
             }
 
             // get the missing range string format
-            res.add(getRange(next, a[i] - 1));
+            res.add(getRange(next, num - 1));
+
+            if (num == Integer.MAX_VALUE) return res;
 
             // now we need to find the next number
-            next = a[i] + 1;
+            next = num + 1;
         }
 
         // do a final check
