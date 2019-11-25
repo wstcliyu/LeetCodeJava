4c524141a861d0890cb232aee11ea47eb65eaa50
YuLi <wstcliyu.gmail.com>
2019-11-25 14:38:46 -0500

    1020 Number of Enclaves

--- Number_1020.java
+++ Number_1020.java
@@ -1,6 +1,53 @@
 class Number_1020 {
-    // My first solution
+    // My updated solution
+    class Result {
+        int area;
+        boolean enclave;
+        Result() {
+            area = 1;
+            enclave = true;
+        }
+    }
+    private int R, C;
+    private int[][] grid;
+    private int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    
+    public int numEnclaves(int[][] grid) {
+        this.grid = grid;
+        R = grid.length;
+        C = grid[0].length;
+        int res = 0;
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (grid[i][j] == 1) {
+                    Result r = dfs(i, j);
+                    res += r.enclave ? r.area : 0;
+                }
+            }
+        }
+        return res;
+    }
+    
+    private Result dfs(int i, int j) {
+        Result r = new Result();
+        if (i == 0 || j == 0 || i == R - 1 || j == C - 1) r.enclave = false;
+        grid[i][j] = 0;
+        for (int[] d : directions) {
+            int x = i + d[0];
+            int y = j + d[1];
+            if (x >= 0 && x < R && y >= 0 && y < C && grid[x][y] == 1) {
+                Result t = dfs(x, y);
+                r.area += t.area;
+                r.enclave &= t.enclave;
+            }
+        }
+        return r;
+    }
 
+
+
+    // My first solution
+    /*
     boolean canGetBound = false;
     
     public int numEnclaves(int[][] A) {
@@ -27,4 +74,5 @@ class Number_1020 {
         A[i][j] = 0;
         return 1 + dfs(i + 1, j, A) + dfs(i - 1, j, A) + dfs(i, j + 1, A) + dfs(i, j - 1, A);
     }
+    */
 }
