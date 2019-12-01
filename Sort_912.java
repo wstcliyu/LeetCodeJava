8cd75613668ffdd9f600e02ff73ae20aad53ff0e
YuLi <wstcliyu.gmail.com>
2019-11-30 20:50:17 -0500

    912 Sort an Array

--- Sort_912.java
+++ Sort_912.java
@@ -4,6 +4,80 @@ class Sort_912 {
         A[i] = A[j];
         A[j] = tmp;
     }
+
+
+    // Heap Sort
+    public int[] sortArray(int[] nums) {
+        buildMaxHeap(nums);
+        heapSort(nums);
+        return nums;
+    }
+
+    private void maxheapify(int[] nums, int i, int size) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        if (l < size && nums[l] > nums[largest]) largest = l;
+        if (r < size && nums[r] > nums[largest]) largest = r;
+        if (largest != i) {
+            swap(nums, i, largest);
+            maxheapify(nums, largest, size);
+        }
+    }
+
+    private void buildMaxHeap(int[] nums) {
+        int n = nums.length;
+        for (int i = n - 1; i >= 0; i--) {
+            maxheapify(nums, i, n);
+        }
+    }
+
+    private void heapSort(int[] nums) {
+        int n = nums.length;
+        for (int i = n - 1; i > 0; i--) {
+            swap(nums, 0, i);
+            maxheapify(nums, 0, i);
+        }
+    }
+
+
+
+    // Merge Sort
+    /*
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void mergeSort(int[] nums, int lo, int hi) {
+        if (lo == hi) return;
+        int m = (lo + hi) / 2;
+        mergeSort(nums, lo, m);
+        mergeSort(nums, m + 1, hi);
+        merge(nums, lo, m, m + 1, hi);
+    }
+    
+    private void merge(int[] nums, int lo1, int hi1, int lo2, int hi2) {
+        int start = lo1;
+        int end = hi2;
+        int[] tmp = new int[end - start + 1];
+        int i = 0;
+        while (lo1 <= hi1 && lo2 <= hi2) {
+            tmp[i++] = nums[lo1] < nums[lo2] ? nums[lo1++] : nums[lo2++];
+        }
+        while (lo1 <= hi1) {
+            tmp[i++] = nums[lo1++];
+        }
+        while (lo2 <= hi2) {
+            tmp[i++] = nums[lo2++];
+        }
+        for (int j = start, k = 0; j <= end; j++, k++) {
+            nums[j] = tmp[k];
+        }
+    }
+    */
+
+
     
     // Bubble Sort
     /*
